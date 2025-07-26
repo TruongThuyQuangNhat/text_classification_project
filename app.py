038126ded6462b30087d4409fce159e4bf6f5fe5
Quang Nhat <truongthuyquangnhat1@gmail.com>
2025-07-26 08:22:56 +0700

    app.run update v2

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def predict():
     prediction = model.predict(vector)[0]
     return jsonify({'label': prediction})
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
